04ac4b284dfbb1d5439c19d754e5b4ad09f78b0c
fenohasina <fenohasinaherimamy@gmail.com>
2026-01-18 17:42:09 +0300

    feat: transformer les grammes en kg

--- src/main/java/UnitEnum.java
+++ src/main/java/UnitEnum.java
@@ -1,5 +1,17 @@
 public enum UnitEnum {
-    PCS,
-    KG,
-    L
+    G(1.0),    // Référence pour la masse
+    KG(1000.0), // 1 kg = 1000 g
+    ML(1.0),   // Référence pour le volume
+    L(1000.0),  // 1 L = 1000 ml
+    PCS(1.0);  // Unité par pièce (pas de conversion)
+
+    private final Double factor;
+
+    UnitEnum(Double factor) {
+        this.factor = factor;
+    }
+
+    public Double getFactor() {
+        return factor;
+    }
 }
