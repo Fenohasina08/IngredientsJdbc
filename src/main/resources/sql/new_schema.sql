6d453272f43934f9cc00409635d47e017db21b0a
fenohasina <fenohasinaherimamy@gmail.com>
2026-01-18 15:45:07 +0300

    feat: alter table de dish et ingredient

--- src/main/resources/sql/new_schema.sql
+++ src/main/resources/sql/new_schema.sql
@@ -1,4 +1,4 @@
-CREATE TYPE unit_type AS ENUM ('PCS',u 'KG', 'L');
+CREATE TYPE unit_type AS ENUM ('PCS','KG', 'L');
 CREATE TABLE DishIngredient (
                                 id SERIAL PRIMARY KEY,
                                 id_dish INT REFERENCES dish(id),
@@ -6,3 +6,5 @@ CREATE TABLE DishIngredient (
                                 quantity_required NUMERIC(10, 2),
                                 unit unit_type
 );
+ALTER TABLE dish ADD COLUMN selling_price NUMERIC(10, 2);
+ALTER TABLE ingredient DROP COLUMN id_dish;
