44bd9be083e223d08f10eff9f9d82d59e6fbd363
fenohasina <fenohasinaherimamy@gmail.com>
2026-01-18 15:32:44 +0300

    feat: add dishingredient table

--- src/main/resources/sql/new_schema.sql
+++ src/main/resources/sql/new_schema.sql
@@ -1,1 +1,8 @@
-CREATE TYPE unit_type AS ENUM ('PCS', 'KG', 'L');
+CREATE TYPE unit_type AS ENUM ('PCS',u 'KG', 'L');
+CREATE TABLE DishIngredient (
+                                id SERIAL PRIMARY KEY,
+                                id_dish INT REFERENCES dish(id),
+                                id_ingredient INT REFERENCES ingredient(id),
+                                quantity_required NUMERIC(10, 2),
+                                unit unit_type
+);
