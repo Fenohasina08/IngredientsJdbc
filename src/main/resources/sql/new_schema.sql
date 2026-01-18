f59c4b3327a50d66e10eaeb23acc1e5a06fb7be7
fenohasina <fenohasinaherimamy@gmail.com>
2026-01-18 16:55:22 +0300

    feat: join

--- src/main/resources/sql/new_schema.sql
+++ src/main/resources/sql/new_schema.sql
@@ -10,7 +10,8 @@ CREATE TABLE DishIngredient (
 ALTER TABLE dish ADD COLUMN selling_price NUMERIC(10, 2);
 ALTER TABLE ingredient DROP COLUMN id_dish;
 
-INSERT INTO DishIngredient (id_dish, id_ingredient, quantity_required, unit) VALUES
+INSERT INTO DishIngredient (id_dish, id_ingredient, quantity_required, unit)
+VALUES
                                                                                  (1, 1, 0.20, 'KG'),
                                                                                  (1, 2, 0.15, 'KG'),
                                                                                  (2, 3, 1.00, 'KG'),
