1c81f81bee45eb6df587d32bbad23ba672f38ef5
fenohasina <fenohasinaherimamy@gmail.com>
2026-01-18 15:55:01 +0300

    feat: update dish table

--- src/main/resources/sql/new_schema.sql
+++ src/main/resources/sql/new_schema.sql
@@ -9,5 +9,13 @@ CREATE TABLE DishIngredient (
 ALTER TABLE dish ADD COLUMN selling_price NUMERIC(10, 2);
 ALTER TABLE ingredient DROP COLUMN id_dish;
 
-INSERT INTO DishIngredient (id_dish, id_ingredient, quantity_required, unit)
-VALUES (1, 2, 0.15, 'KG');
+INSERT INTO DishIngredient (id_dish, id_ingredient, quantity_required, unit) VALUES
+                                                                                 (1, 1, 0.20, 'KG'),
+                                                                                 (1, 2, 0.15, 'KG'),
+                                                                                 (2, 3, 1.00, 'KG'),
+                                                                                 (4, 4, 0.30, 'KG'),
+                                                                                 (4, 5, 0.20, 'KG');
+
+UPDATE dish SET selling_price = 3500.00 WHERE id = 1;
+UPDATE dish SET selling_price = 12000.00 WHERE id = 2;
+UPDATE dish SET selling_price = 8000.00 WHERE id = 4;
